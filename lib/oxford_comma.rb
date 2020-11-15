7a63b5d881fa68a97654bf6325d6ba8c95e83978
sbb3391 <sbb33391@gmail.com>
2020-11-15 18:33:45 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.count() == 1
+        array.join
+    elsif array.count() == 2
+        array.join(" and ")
+    else array.count() > 2
+        array_copy = array
+        array_copy[-1] = "and #{array_copy[-1]}"
+        array_copy.join(", ")
+    end
 end
